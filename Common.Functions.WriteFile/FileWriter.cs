45d69b394a04b94332c581a5a439f3aa65a1591b
Christopher Dorst <chris@dorst.email>
2017-12-25 19:35:38 -0600

    Adding FileWriter helper

---
+++ Common.Functions.WriteFile/FileWriter.cs
@@ -0,0 +1,23 @@
+﻿using Common.Functions.CreateDirectory;
+using static System.IO.File;
+using static System.IO.Path;
+
+namespace Common.Functions.WriteFile
+{
+    public static class FileWriter
+    {
+        public static void WriteFile(string directoryPath, string fileName, string content)
+        {
+            CreateDirectory(directoryPath);
+            WriteAllText(Combine(directoryPath, fileName), content);
+        }
+
+        public static void WriteFile(string directoryPath, string fileName, byte[] content)
+        {
+            CreateDirectory(directoryPath);
+            WriteAllBytes(Combine(directoryPath, fileName), content);
+        }
+
+        private static void CreateDirectory(string path) => DirectoryCreator.Create(path);
+    }
+}

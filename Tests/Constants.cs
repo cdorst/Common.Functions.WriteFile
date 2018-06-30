e3482e3e51c405e0cb15194626a63b190267b08f
Christopher Dorst <chris@dorst.email>
2018-06-30 15:54:23 -0500

    Use APPVEYOR_BUILD_FOLDER in path calculation if available

--- Tests/Constants.cs
+++ Tests/Constants.cs
@@ -1,6 +1,8 @@
 ﻿using static System.IO.Directory;
 using static System.IO.Path;
 using static System.Guid;
+using static System.Environment;
+using static System.String;
 
 namespace Tests
 {
@@ -10,8 +12,19 @@ namespace Tests
         public const string Name = "TestFile.txt";
 
         public static readonly byte[] Bytes = { 1, 2, 3, 4 };
-        public static readonly string Path = Combine(GetCurrentDirectory(), NewGuid().ToString());
+        public static readonly string Path = GetPath();
 
         public static readonly string FullPath = Combine(Path, Name);
+
+        private static string GetPath()
+        {
+            var directory = GetEnvironmentVariable("APPVEYOR_BUILD_FOLDER");
+            return IsNullOrEmpty(directory)
+                ? GetPathCombination(GetCurrentDirectory())
+                : GetPathCombination(directory);
+        }
+
+        private static string GetPathCombination(in string directory)
+            => Combine(directory, NewGuid().ToString());
     }
 }

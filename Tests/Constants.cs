0ad1570b4e947688b5b75b5d900c58e8fae73890
Christopher Dorst <chris@dorst.email>
2018-06-30 15:44:05 -0500

    Add test constants

---
+++ Tests/Constants.cs
@@ -0,0 +1,17 @@
+﻿using static System.IO.Directory;
+using static System.IO.Path;
+using static System.Guid;
+
+namespace Tests
+{
+    internal static class Constants
+    {
+        public const string Contents = "Hello, world!";
+        public const string Name = "TestFile.txt";
+
+        public static readonly byte[] Bytes = { 1, 2, 3, 4 };
+        public static readonly string Path = Combine(GetCurrentDirectory(), NewGuid().ToString());
+
+        public static readonly string FullPath = Combine(Path, Name);
+    }
+}

00c959d290db85628a6446ca0c13ef89c44488d9
Christopher Dorst <chris@dorst.email>
2018-06-30 15:44:15 -0500

    Add test scenario helper functions

---
+++ Tests/Helpers.cs
@@ -0,0 +1,20 @@
+﻿using static Xunit.Assert;
+using static Tests.Constants;
+using static System.IO.File;
+using static Common.Functions.ClearDirectory.DirectoryClearer;
+
+namespace Tests
+{
+    internal static class Helpers
+    {
+        public static void Arrange() => Clear(Path);
+
+        public static void AssertFileExists() => True(Exists(FullPath));
+
+        public static void Cleanup()
+        {
+            Clear(Path);
+            System.IO.Directory.Delete(Path);
+        }
+    }
+}
